ccd7c1e36c39f3b89c89ab5898c0bb9375a3f7b5
Brad Searle <bobthebutcher@users.noreply.github.com>
2016-01-27 19:57:18 +1100

    added method to add an internal user

--- cream.py
+++ cream.py
@@ -7,6 +7,8 @@ requests
 xmltodict
  - https://github.com/martinblech/xmltodict
 """
+import os
+dir = os.path.dirname(__file__)
 
 import json
 import requests
@@ -14,21 +16,20 @@ import xmltodict
 
 
 class ERS(object):
-
-    def __init__(self, ise_node, user_name, user_pass, verify=False, disable_warnings=False, timeout=2):
+    def __init__(self, ise_node, ers_user, ers_pass, verify=False, disable_warnings=False, timeout=2):
         """
 
         :param ise_node:
-        :param user_name:
-        :param user_pass:
+        :param ers_user:
+        :param ers_pass:
         :param verify:
         :param disable_warnings:
         :param timeout:
         :return:
         """
         self.ise_node = ise_node
-        self.user_name = user_name
-        self.user_pass = user_pass
+        self.user_name = ers_user
+        self.user_pass = ers_pass
 
         self.url_base = 'https://{0}:9060/ers'.format(self.ise_node)
         self.ise = requests.session()
@@ -58,7 +59,96 @@ class ERS(object):
         })
 
         resp = self.ise.get('{0}/config/internaluser'.format(self.url_base))
-        result = [(i['@name'], i['@id']) for i in ERS._to_json(resp.text)['ns3:searchResult']['resources']['resource']]
-        return result
 
+        result = {
+            'success': False,
+            'response': '',
+            'error': '',
+        }
 
+        if resp.status_code == 200:
+            result['success'] = True
+            result['response'] = [(i['@name'], i['@id'])
+                                  for i in ERS._to_json(resp.text)['ns3:searchResult']['resources']['resource']]
+            return result
+        else:
+            result['response'] = resp.text
+            result['error'] = resp.status_code
+            return result
+
+    def get_user(self, user_oid):
+        """
+
+        :param user_oid:
+        :return:
+        """
+        self.ise.headers.update({
+            'Accept': 'application/vnd.com.cisco.ise.identity.internaluser.1.1+xml',
+            'Connection': 'keep_alive',
+        })
+
+        resp = self.ise.get('{0}/config/internaluser/{1}'.format(self.url_base, user_oid))
+
+        result = {
+            'success': False,
+            'response': '',
+            'error': '',
+        }
+
+        if resp.status_code == 200:
+            result['success'] = True
+            result['response'] = ERS._to_json(resp.text)['ns4:internaluser']
+            return result
+        elif resp.status_code == 404:
+            result['response'] = 'Unknown User'
+            result['error'] = resp.status_code
+            return result
+        else:
+            result['response'] = resp.text
+            result['error'] = resp.status_code
+            return result
+
+    def add_user(self,
+                 user_id,
+                 password,
+                 enable='',
+                 first_name='',
+                 last_name='',
+                 email='',
+                 description='',
+                 user_group_oid=''):
+        """
+        Add a user to the local user store
+        :param user_id: User ID
+        :param password: User password
+        :param enable: Enable password used for Tacacs
+        :param first_name: First name
+        :param last_name: Last name
+        :param email: email address
+        :param description: User description
+        :param user_group_oid: OID of group to add user to
+        :return: result dictionary
+        """
+        result = {
+            'success': False,
+            'response': '',
+            'error': '',
+        }
+
+        headers = {'Content-Type': 'application/vnd.com.cisco.ise.identity.internaluser.1.1+xml; charset=utf-8'}
+
+        data = open(os.path.join(dir, 'xml/user_add.xml'), 'r').read().format(
+                user_id, password, enable, first_name, last_name, email, description, user_group_oid)
+
+        url = '{0}/config/internaluser'.format(self.url_base)
+
+        resp = self.ise.post(url, data=data, headers=headers, timeout=self.timeout)
+
+        if resp.status_code == 201:
+            result['success'] = True
+            result['response'] = '{0} Added Successfully'.format(user_id)
+            return result
+        else:
+            result['response'] = resp.text
+            result['error'] = resp.status_code
+            return result
